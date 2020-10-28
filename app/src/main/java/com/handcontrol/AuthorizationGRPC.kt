099a154a32a314622a35d4ea3bc1dee8c383abd3
VladimirRozov <39130781+VladimirRozov@users.noreply.github.com>
2020-10-28 19:02:02 +0300

    grpc

--- app/src/main/java/com/handcontrol/AuthorizationGRPC.kt
+++ app/src/main/java/com/handcontrol/AuthorizationGRPC.kt
@@ -9,39 +9,43 @@ import java.util.concurrent.TimeUnit
 class AuthorizationGRPC(private val channel: ManagedChannel) : Closeable {
     var login_name = ""
     var password = ""
-    var imei = "";
+    var imei = ""
+    var user_name = ""
 
     private val stub: HandleRequestCoroutineStub = HandleRequestCoroutineStub(channel) {
-        suspend fun greet(name: String) {
-            val request = login.newBuilder().build()
+        suspend fun loginAuth() {
+            val request = login.newBuilder().setImei(imei).setLogin(login_name).setPassword(password).build()
             val response = stub.login(request)
             println("Received: ${response.message}")
         }
 
-
-
-        fun getIMEI(context: Context): String? {
-            val mngr = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-            imei = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                mngr.getImei()
-            } else ({
-                // mngr.getDeviceId()
-                0
-            }).toString()
-            return imei;
-        }
-
-        fun login(login: String, password: String) {
-            this.login_name = login
-            this.password = password
-        }
-
-        fun registration() {
-
+        suspend fun clientAuth() {
+            val request = ProRequest.newBuilder().setImei(imei).setRequest(user_name).build()
+            val response = stub.ProRequest(request)
+            println("Received: ${response.message}")
         }
     }
 
     override fun close() {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+    }
+
+    fun getIMEI(context: Context): String? {
+        val mngr = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        imei = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            mngr.imei
+        } else ({
+            mngr.deviceId
+        }).toString()
+        return imei;
+    }
+
+    fun login_user(login: String, password: String) {
+        this.login_name = login
+        this.password = password
+    }
+
+    fun registration(user: String) {
+        this.user_name = user;
     }
 }
